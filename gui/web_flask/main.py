f9db0ed4cfcc935e817c5f96cbb2fb3054d52ef5
Bashir M <bmohammed@lbl.gov>
2021-07-18 00:18:08 -0700

    updated  netpredict GUI flask version2

--- gui/web_flask/main.py
+++ gui/web_flask/main.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, request
-import json
-import pandas as pd
-import matplotlib.pyplot as plt
 
-from network_map import graph_main, graph_bar
+from network_map import graph_main, graph_bar, graph_nodes, nodes_join_line, Shortest_path_graph
 
 app = Flask(__name__)
 sites = [
-    '',
     'ALBQ',
     'AMES',
     'AMST',
@@ -105,7 +101,6 @@ predictionsData = [
 #     ['2017-01-24T23:00', 0.28, ['SACR', 'DENV']],
 # ]
 dataTransferredOptions = [
-    '',
     'Less than 1GB',
     '1GB -  10GB',
     '10GB - 20GB',
@@ -116,21 +111,26 @@ dataTransferredOptions = [
     '100GB - 120GB'
 ]
 
+graph_type=[
+    'Hourly',
+    'Weekly',
+    'Next 3 Months'
+]
 # map_data = json.load(open('map-data.json'))
-# print(map_data)
+# # print(map_data)
 # coordinates_location = []
 # for city in map_data["data"]['mapTopology']['nodes']:
 #     coordinates_location.append([city['name'], city['x'] * 10 + 150, city['y'] * 9 + 100])
 #     # plt.annotate(city['name'],xy=city['name'])
 # df = pd.DataFrame(coordinates_location, columns=['Name', 'X', 'Y'])
-# print(df.to_numpy().resize())
+# # print(df.to_numpy().resize())
 # plt.scatter('X','Y',data=df)
 # for i in range(df.shape[0]):
 #     plt.text(x=df.X[i]+0.5,y=df.Y[i]+0.5 , s=df.Name[i],
 #              fontdict=dict(color='red', size = 4))
 #                 # bbox = dict(facecolor='yellow', alpha = 0.5))
 #
-# # print(plt.imread('static/images/NetPredict-05.png')[:,:,0])
+# # # print(plt.imread('static/images/NetPredict-05.png')[:,:,0])
 # plt.imshow(plt.imread('static/images/NetPredict-05.png'))
 # plt.tick_params(left = False, right = False , labelleft = False ,
 #                 labelbottom = False, bottom = False)
@@ -141,26 +141,115 @@ dataTransferredOptions = [
 # plt.axis('off')
 # plt.savefig('static/images/out.png',dpi=720)
 # # # plt.show()
-
+# graph=['14 may', '15 may', '16 may', '17 may', '18 may', '19 may', '20 may', '21 may']
+# predict=[0.11, 0.13, 0.07, 0.04, 0.33, 0.66, 0.95, 0.6]
 graph_main()
 graph_bar(predictionsData)
 
-@app.route('/home')
+Graph_Nodes=graph_nodes()
+Graph_node_line=nodes_join_line()
+graph,predict,barColors,shortest_path_data=[],[],[],[]
+source,destination,data_transmit,gr_type='','','',''
+@app.route('/')
 def home():
-    return render_template("index.html", data=sites, data_transfer=dataTransferredOptions, prediction_data=predictionsData)
+    print(graph,predict,barColors)
+    return render_template("index.html",
+                           data_source=sites,
+                           data_destination=sites.copy(),
+                           source=source, destination=destination,
+                           data_transmit=data_transmit,
+                           data_transfer=dataTransferredOptions,
+                           prediction_data=predictionsData,
+                           graph_type=graph_type,
+                           Graph_Nodes=Graph_Nodes,
+                           graph_date=graph,
+                           graph_predict=predict,
+                           graph_color=barColors,
+                           Graph_node_line=Graph_node_line,
+                           Shortest_Path=[])
+
+@app.route('/home',methods=['GET','POST'])
+def updated_home():
+    global graph
+    global predict
+    global barColors
+    global shortest_path_data
+    global source
+    global destination
+    global data_transmit
+    global gr_type
+    global data_transfer
+    if request.method=='POST':
+        print('*****************',request.form)
+        source,destination,data_transfer,gr_type = request.form['source'],request.form['destination'],\
+                                                request.form['data_transfer'],request.form['data_type']
+
+        barColors = ["#f00", "#0f0", "blue", "orange",
+                     "brown", "yellow", "purple", "pink",
+                     "black","violet","gray","silver",]
+        if gr_type=='Hourly':
+            graph = ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00','09:00', '10:00', '11:00', '12:00',]
+            predict = [0.11, 0.13, 0.07, 0.04, 0.33, 0.66, 0.95, 0.6, 0.33, 0.66, 0.95, 0.6]
+        elif gr_type=='Weekly':
+            graph = ['14 may', '15 may', '16 may', '17 may', '18 may', '19 may', '20 may', '21 may']
+            predict = [0.11, 0.13, 0.07, 0.04, 0.33, 0.66, 0.95, 0.6]
+            # barColors = ["red", "green", "blue", "orange", "brown", "yellow", "purple", "pink"]
+        elif gr_type=='Next':
+            graph = ['July', 'August', 'September']
+            predict = [0.11, 0.13, 0.12]
+            # barColors = ["red", "green", "blue"]
+            # print('------------------inside')
+        else:
+            graph = []
+            predict = []
+            barColors = []
+
+        shortest_path_data = Shortest_path_graph(source, destination)
+        # print("----------", shortest_path_data)
+        # print("**********",list(set(Graph_node_line)-set(shortest_path_data)))
+        return render_template("index.html",
+               data_source=sites,
+               data_destination=sites.copy(),
+               source=source, destination=destination,
+               data_transmit=data_transmit,
+               gr_type=gr_type,
+               data_transfer=dataTransferredOptions,
+               prediction_data=predictionsData,
+               graph_type=graph_type,
+               graph_date=graph,
+               graph_predict=predict,
+               graph_color=barColors[:len(predict)],
+               Graph_node_line=Graph_node_line,
+               Shortest_Path=shortest_path_data,
+               Graph_Nodes=Graph_Nodes,
+               path_color='blue'
+               )
 
 
-@app.route('/home', methods=['POST'])
-def optimize_path():
-    if request.method == 'POST':
-        source = request.form['source']
-        destination = request.form['destination']
+@app.route('/optimize_home/<source>/<destination>/<traffic>')
+def optimize_path(source,destination,traffic):
 
-        if source == destination:
-            return "<h1>Source and destination can't be same. Please select the different cities.</h1>"
-        return str(source) + " " + destination
-    # elif request.method=='GET':
-    #     request.form
+    if float(traffic)>=0.5:
+        color='red'
+    else:
+        color='blue'
+    print('*****************', request.form)
+    return render_template("index.html",
+                           data_source=sites,
+                           data_destination=sites.copy(),
+                           source=source, destination=destination,
+                           data_transmit=data_transmit,
+                           gr_type=gr_type,
+                           data_transfer=dataTransferredOptions,
+                           prediction_data=predictionsData,
+                           graph_type=graph_type,
+                           Graph_Nodes=Graph_Nodes,
+                           graph_date=graph,
+                           graph_predict=predict,
+                           graph_color=barColors,
+                           Graph_node_line=Graph_node_line,
+                           Shortest_Path=Shortest_path_graph(source, destination),
+                           path_color=color)
 
 
 if __name__ == "__main__":
--- gui/web_flask/network_map.py
+++ gui/web_flask/network_map.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import LabelEncoder
 
 import networkx as nx
 from matplotlib import pyplot as plt
+
+from shortest_path import Graph, dijsktra
+
 predictionsData = [
     ['14 may', 0.01, ['FNAL', 'STAR']],
     ['15 may', 0.13, ['NREL', 'DENV', 'KANS']],
@@ -18,7 +21,7 @@ predictionsData = [
 
 def graph_bar(data):
     date,value=[i[0] for i in data],[i[1] for i in data]
-    print(date,value)
+    # print(date,value)
     plt.bar(date,value)
     plt.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
     plt.xlabel("Date")
@@ -35,18 +38,19 @@ def graph_main():
     coordinates_location = []
     for city in map_data["data"]['mapTopology']['nodes']:
         coordinates_location.append([city['name'], city['x'] * 10 + 150, city['y'] * 9 + 100])
-    print(coordinates_location)
+    # print(coordinates_location)
     df = pd.DataFrame(coordinates_location, columns=['Name', 'X', 'Y'])
+    # print(df)
     names=LabelEncoder()
     label_transform=names.fit_transform(df['Name'])
     names_classes={j:i for i,j in zip(label_transform,names.classes_)}
-    print(names_classes)
+    # print(names_classes)
     edges=[]
     for edge in map_data["data"]['mapTopology']['edges']:
         # for i in x:
         a,b=edge['name'].split('--')
         edges.append((names_classes[a],names_classes[b],1))
-    print(edges)
+    # print(edges)
     G = nx.Graph()
     points = [(i,1200-j) for (c,i,j) in coordinates_location]#[(1, 10), (8, 10), (10, 8), (7, 4), (3, 1)]  # (x,y) points
     # edges = [(0, 1, 10), (1, 2, 5), (2, 3, 25), (0, 3, 3), (3, 4, 8)]  # (v1,v2, weight)
@@ -57,7 +61,7 @@ def graph_main():
     # you want your own layout
     # pos = nx.spring_layout(G)
     pos = {point: point for point in points}
-    print(pos)
+    # print(pos)
     # add axis
     fig, ax = plt.subplots()
     for i in range(df.shape[0]):
@@ -78,6 +82,75 @@ def graph_main():
     # mng.full_screen_toggle()
     plt.savefig('static/images/out.png', dpi=720,bbox_inches='tight')
     plt.close()
-    # plt.show()
+    plt.show()
 
+def graph_nodes():
+    map_data = json.load(open('map-data.json'))
+    coordinates_location = []
+    for city in map_data["data"]['mapTopology']['nodes']:
+        coordinates_location.append([city['name'], city['x'], city['y']])
+    # # print(coordinates_location)
+    df = pd.DataFrame(coordinates_location, columns=['Name', 'X', 'Y'])
+    return df.to_numpy()
+
+def nodes_join_line():
+    map_data = json.load(open('map-data.json'))
+    edges = []
+    nodes_of_sites=[{i[0]: (i[1], i[2])} for i in graph_nodes()]
+    nodes_of_site={}
+    [nodes_of_site.update(i) for i in nodes_of_sites]
+    # print(nodes_of_site)
+    line_coordinates=[]
+    for edge in map_data["data"]['mapTopology']['edges']:
+        # for i in x:
+        a, b = edge['name'].split('--')
+        line_coordinates.append((a,nodes_of_site[a],b,nodes_of_site[b]))
+        # print(a,b)
+    return line_coordinates
+
+def Shortest_path_graph(source,destination):
+    # map_data = json.load(open('map-data.json'))
+    # edges = []
+    nodes_of_sites=[{i[0]: (i[1], i[2])} for i in graph_nodes()]
+    nodes_of_site={}
+    [nodes_of_site.update(i) for i in nodes_of_sites]
+    # print(nodes_of_site)
+    Shortest_path_coordinates=[]
+
+    graph = Graph()
+    edges = [('AMES', 'STAR', 1), ('BOIS', 'INL', 1), ('PNNL', 'PNWG', 1), ('BOIS', 'PNWG', 1),
+             ('EQX-ASH', 'EQX-CHI', 1), ('LIGO', 'PNWG', 1), ('BOST', 'PSFC', 1), ('KANS', 'KCNSC', 1),
+             ('ATLA', 'ORAU', 1), ('BOST', 'LNS', 1), ('AMST', 'BOST', 1), ('NERSC', 'SUNN', 1), ('JLAB', 'WASH', 1),
+             ('ATLA', 'SRS', 1), ('GA', 'SUNN', 1), ('HOUS', 'PANTEX', 1), ('EQX-ASH', 'NETL-PGH', 1),
+             ('FNAL', 'STAR', 1), ('LBNL', 'NPS', 1), ('HOUS', 'KANS', 1), ('ATLA', 'ETTP', 1), ('CHIC', 'KANS', 1),
+             ('ALBQ', 'DENV', 1), ('JGI', 'SACR', 1), ('LSVN', 'SUNN', 1), ('LBNL', 'SUNN', 1), ('ALBQ', 'KCNSC-NM', 1),
+             ('CHIC', 'STAR', 1), ('DENV', 'LSVN', 1), ('DENV', 'NREL', 1), ('ATLA', 'Y12', 1), ('DENV', 'KANS', 1),
+             ('DENV', 'NGA-SW', 1), ('LSVN', 'NNSS', 1), ('LLNL', 'SUNN', 1), ('HOUS', 'NASH', 1), ('PNWG', 'SACR', 1),
+             ('CHIC', 'WASH', 1), ('LOND', 'NEWY', 1), ('CERN-513', 'CERN-773', 1), ('ATLA', 'NASH', 1),
+             ('AMST', 'CERN-513', 1), ('CERN', 'CERN-513', 1), ('ANL', 'STAR', 1), ('PPPL', 'WASH', 1),
+             ('SLAC', 'SUNN', 1), ('ATLA', 'ORNL', 1), ('BOST', 'STAR', 1), ('ALBQ', 'LANL', 1), ('NASH', 'WASH', 1),
+             ('EQX-ASH', 'WASH', 1), ('AMST', 'LOND', 1), ('AOFA', 'STAR', 1), ('AOFA', 'WASH', 1), ('ELPA', 'HOUS', 1),
+             ('ELPA', 'SUNN', 1), ('ALBQ', 'SNLA', 1), ('SACR', 'SUNN', 1), ('CERN-773', 'LOND', 1),
+             ('CHIC', 'NASH', 1), ('CERN-513', 'WASH', 1), ('DENV', 'PNWG', 1), ('EQX-ASH', 'NETL-MGN', 1),
+             ('AOFA', 'LOND', 1), ('BNL', 'NEWY', 1), ('CHIC', 'EQX-CHI', 1), ('ATLA', 'WASH', 1), ('BOIS', 'DENV', 1),
+             ('AOFA', 'NEWY', 1), ('BOST', 'NEWY', 1), ('ALBQ', 'ELPA', 1), ('DENV', 'SACR', 1), ('SACR', 'SNLL', 1)]
+    
+    for edge in edges:
+        graph.add_edge(*edge)
+    #labels = {LBNL:'LBNL',SLAC:'SLAC'}
+    path_source_destination=dijsktra(graph, source,destination)
+    # print(path_source_destination)
+    for (index,edge) in enumerate(path_source_destination):
+        # for i in x:
+        try:
+            a, b = edge,path_source_destination[index+1]     #edge['name'].split('--')
+        except IndexError:
+            continue
+        Shortest_path_coordinates.append((a,nodes_of_site[a],b,nodes_of_site[b]))
+        # # print(a,b)
+    return Shortest_path_coordinates
+# # print(Shortest_path_graph())
+# nodes_join_line()
 # graph_bar(predictionsData)
+# graph_main()
+# # print(graph_nodes())

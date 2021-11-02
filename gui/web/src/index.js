ee96ed058e093cdfa002c2b4f69d44c5d6063f26
mariamkiran <mariam.kiran@gmail.com>
2021-11-01 20:10:53 -0700

    Delete gui directory

--- gui/web/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
-
-import 'antd/dist/antd.css';
-import './index.css';
-
-ReactDOM.render(<Home />, document.getElementById('root'));

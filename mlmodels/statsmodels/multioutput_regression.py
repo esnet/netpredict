d2e5b3a8fb968ef0d1ceccef1a9f2b4e9d062aab
Mariam Kiran <mariamkiran@gmail.com>
2021-11-17 19:18:56 -0800

    multioutput_regression.py

--- mlmodels/statsmodels/multioutput_regression.py
+++ mlmodels/statsmodels/multioutput_regression.py
@@ -3,6 +3,7 @@
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 test_samplex=[0.21947749, 0.32948997, 0.81560036, 0.440956, -0.0606303]
 test_sampley=[-0.29257894, -0.2820059]
@@ -14,30 +15,35 @@ test_sampley=[-0.29257894, -0.2820059]
 def NP_LinearRegression(x):
     # create datasets
 
-    print("HHHH")
-    print(x)
+    
     #split incoming data into X and y 
     #x_train, x_test, y_train, y_test = train_test_split(x, y,  test_size=2, random_state=0)
     # define model
-    X, y = make_regression(n_samples=10, n_features=1, n_informative=5, n_targets=5, random_state=1, noise=0.5)
+    X, y = make_regression(n_samples=10, n_features=11, n_informative=5, n_targets=5, random_state=1, noise=0.5)
     # define model
     # fit model
-    print("1HHHH")
-    print(x)
-    print("ffff")
-    print(y)
     model = LinearRegression()
 
     # fit model
     model.fit(X, y)
 
-    print("2HHHH")
-
+    print("Predictions...")
+    predDf = pd.DataFrame(index=[0],columns=x)
+    for col in predDf.columns:
+        predDf[col]=predDf[col].fillna(0)
+    #print(predDf)
     # make a prediction
-    row = [-0.00290545]
-    yhat = model.predict([row])
-    # summarize prediction
-    print(yhat[0])
+    for colname in x:
+        ys=x[colname]
+        
+        y_hats2 = model.predict([ys])
+        print("For link id: ",colname)
+        print(y_hats2)
 
+        #predDf=predDf.append(predDf.loc[colname], ignore_index=True)
+
+        #predDf[colname].append(y_hats2)
+
+        #predDf = pd.concat([predDf,y_hats2])
 
 #NP_LinearRegression(test_samplex, test_sampley)

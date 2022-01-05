400b91342bb3deab11a02856d4eb9d71e44419b7
Mariam Kiran <mariamkiran@gmail.com>
2022-01-05 12:43:43 -0800

    Adding DCRNN model files

--- mlmodels/spatio-temporalGCN/main.py
+++ mlmodels/spatio-temporalGCN/main.py
@@ -1,3 +1,5 @@
+# This is an implementation to DCRNN by Yu et al.
+
 import dgl
 import random
 import torch
@@ -12,8 +14,19 @@ import torch.nn as nn
 import argparse
 import scipy.sparse as sp
 
-graph_link_ids="" #txt
-link_weights="" #csv
+########################################
+#data files:
+# Create the adjacency matrix as a Graph in DGL:
+## graph_sensors_ids.txt (for Id of sensors) === esnet_links_id.txt
+## distances_la_so12.csv for distance between sensors  === link_capacity.csv
+# actual speed data
+# metr_la.h5 shape [x,y], where x shows number of timesteps and y is the number of sensors- GNN will use
+# this histroical speed to predict future speed
+
+########################################
+
+graph_link_ids="../../datasets/snmp_esnet/esnet_links_ids.txt" #txt
+link_bw_capacity="../../datasets/snmp_esnet/link_capacity.csv" #csv
 link_data="" #h5
 
 n_window_size=144
@@ -21,7 +34,7 @@ save_path=""
 predicted_len=20
 
 
-
+"""
 def trainSTGCN():
     device = torch.device("cuda") if torch.cuda.is_available() and not args.disablecuda else torch.device("cpu")
 
@@ -37,4 +50,19 @@ def trainSTGCN():
     df = pd.read_hdf(link_data)
     num_samples, num_nodes = df.shape
     tsdata = df.to_numpy()
+"""
+def main():
+    #prepare and load the data set
+    with open(graph_link_ids) as f:
+        link_ids = f.read().strip().split(',')
 
+    print(link_ids)
+
+    #bwcap_df=pd.read_csv(link_weights, dtype={'from': 'str', 'to': 'str'})
+
+    """adj_mx = get_adjacency_matrix(bwcap_df, link_ids)
+    sp_mx = sp.coo_matrix(adj_mx)
+    G = dgl.from_scipy(sp_mx)
+    """
+
+main()
